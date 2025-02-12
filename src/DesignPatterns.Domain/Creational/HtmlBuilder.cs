021b36c577f97a58d2a91fd2973128a5be3c0b7a
WIN-F7470BLT8P0\kamrui-pc <alexjwatts83@gmail.com>
2025-02-12 15:34:52 +1100

    initial checkin

---
+++ src/DesignPatterns.Domain/Creational/HtmlBuilder.cs
@@ -0,0 +1,27 @@
+﻿namespace DesignPatterns.Domain.Creational;
+
+public class HtmlBuilder
+{
+    private readonly HtmlElement root;
+
+    public HtmlBuilder(string rootName)
+    {
+        root = new HtmlElement()
+        {
+            Name = rootName
+        };
+    }
+
+    public HtmlBuilder WithChild(string childName, string childText)
+    {
+        var e = new HtmlElement(childName, childText);
+        root.Elements.Add(e);
+        return this;
+    }
+
+    public HtmlElement Build()
+    {
+        return root;
+    }
+}
+
