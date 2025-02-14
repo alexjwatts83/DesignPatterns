c7793297a4cdf0e2051d3ae83bc51b999c796290
WIN-F7470BLT8P0\kamrui-pc <alexjwatts83@gmail.com>
2025-02-15 10:20:28 +1100

    added helper

---
+++ src/DesignPatterns.Domain/Other/ReplaceDigitsHelper.cs
@@ -0,0 +1,14 @@
+﻿namespace DesignPatterns.Domain.Other;
+public static class ReplaceDigitsHelper
+{
+    // TODO: fix this method - fix bugs, make more efficient, and return correct result
+    public static string ReplaceDigits(string sentence)
+    {
+        string[] words = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+        for (var i = 0; i < words.Length; i++)
+        {
+            sentence = sentence.Replace(i.ToString(), words[i]);
+        }
+        return sentence;
+    }
+}
