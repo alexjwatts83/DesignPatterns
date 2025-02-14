8af25bf868fa534242181e4a9259a3a1ea911ecd
WIN-F7470BLT8P0\kamrui-pc <alexjwatts83@gmail.com>
2025-02-15 10:44:30 +1100

    replace text implemented

--- src/DesignPatterns.Domain/Other/ReplaceDigitsHelper.cs
+++ src/DesignPatterns.Domain/Other/ReplaceDigitsHelper.cs
@@ -1,14 +1,28 @@
-﻿namespace DesignPatterns.Domain.Other;
+﻿using System.Text;
+
+namespace DesignPatterns.Domain.Other;
 public static class ReplaceDigitsHelper
 {
     // TODO: fix this method - fix bugs, make more efficient, and return correct result
     public static string ReplaceDigits(string sentence)
     {
         string[] words = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
-        for (var i = 0; i < words.Length; i++)
+        var sentenceBuilder = new StringBuilder();
+
+        for (var i = 0; i < sentence.Length; i++)
         {
-            sentence = sentence.Replace(i.ToString(), words[i]);
+            var letter = sentence[i].ToString();
+            if (int.TryParse(letter, out int num))
+            {
+                var word = words[num];
+                sentenceBuilder.Append(word);
+            }
+            else
+            {
+                sentenceBuilder.Append(letter);
+            }
         }
-        return sentence;
+
+        return sentenceBuilder.ToString();
     }
 }
