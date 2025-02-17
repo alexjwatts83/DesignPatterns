9de799186f25e3d019aecac8128535bd082b91fd
WIN-F7470BLT8P0\kamrui-pc <alexjwatts83@gmail.com>
2025-02-17 11:16:38 +1100

    finished off the matrix helper class

---
+++ src/DesignPatterns.Domain.Tests/Other/MatrixHelperTests.cs
@@ -0,0 +1,45 @@
+﻿using Xunit;
+using Shouldly;
+
+namespace DesignPatterns.Domain.Other.Tests;
+
+public class MatrixHelperTests
+{
+    [Fact()]
+    public void MultiplyTest()
+    {
+        // arrange
+        var a = new Matrix<int>(2, 3);
+        var b = new Matrix<int>(3, 2);
+
+        a[0, 0] = 1;
+        a[0, 1] = 2;
+        a[0, 2] = 3;
+        a[1, 0] = 4;
+        a[1, 1] = 5;
+        a[1, 2] = 6;
+
+
+        b[0, 0] = 7;
+        b[0, 1] = 8;
+
+        b[1, 0] = 9;
+        b[1, 1] = 10;
+
+        b[2, 0] = 11;
+        b[2, 1] = 12;
+
+        // act
+        var result = MatrixHelper.Multiply(a, b);
+
+        // assert
+        var expected = new Matrix<int>(2, 2);
+        expected[0, 0] = 58;
+        expected[0, 1] = 64;
+
+        expected[1, 0] = 139;
+        expected[1, 1] = 154;
+
+        result.ShouldBe(expected);
+    }
+}
--- src/DesignPatterns.Domain/Other/MatrixHelper.cs
+++ src/DesignPatterns.Domain/Other/MatrixHelper.cs
@@ -3,42 +3,59 @@
 namespace DesignPatterns.Domain.Other;
 
 // TODO: optimize this class
-public class Matrix : IEnumerable
+public class Matrix<T> : IEnumerable<T>
 {
-    private int[,] _data;
+    private readonly T[] _data;
 
-    public int Rows { get { return _data.GetLength(0); } }
-    public int Columns { get { return _data.GetLength(1); } }
-
-    public int this[int row, int col]
+    public Matrix(int rows, int cols)
     {
-        get { return _data[row, col]; }
-        set { _data[row, col] = value; }
+        Rows = rows;
+        Columns = cols;
+        _data = new T[Rows * Columns];
+    }
+
+    public int Rows { get; private set; }
+    public int Columns { get; private set; }
+
+    public T this[int row, int col]
+    {
+        get { return _data[col * Rows + row]; }
+        set { _data[col * Rows + row] = value; }
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        return GetEnumerator();
     }
 
     IEnumerator IEnumerable.GetEnumerator()
     {
-        return _data.GetEnumerator();
-    }
-
-    public Matrix(int[,] value)
-    {
-        _data = value;
+        for (int j = 0; j < Columns; j++) 
+        {
+            for (int i = 0; i < Rows; i++)
+                yield return _data[j * Rows + i];
+        }
     }
 }
 
 public class MatrixHelper
 {
-    public static Matrix Multiply(Matrix a, Matrix b)
+    public static Matrix<int> Multiply(Matrix<int> a, Matrix<int> b)
     {
-        var result = new Matrix(new int[a.Rows, b.Columns]);
+        var result = new Matrix<int>(a.Rows, b.Columns);
         for (int i = 0; i < result.Rows; i++)
         {
             for (int j = 0; j < result.Columns; j++)
             {
                 result[i, j] = 0;
                 for (int k = 0; k < a.Columns; k++)
+                {
+                    var aValue = a[i, k];
+                    var bValue = b[k, j];
+
                     result[i, j] += (a[i, k] * b[k, j]);
+                }
+                    
             }
         }
         return result;
