e3a8a451986d8029fd9eae953a52de9f89498bcc
WIN-F7470BLT8P0\kamrui-pc <alexjwatts83@gmail.com>
2025-02-25 09:52:20 +1100

    wip checkin

---
+++ src/DesignPatterns.Domain/Creational/Builder/CreationalBuilder.cs
@@ -0,0 +1,40 @@
+﻿namespace DesignPatterns.Domain.Creational.Builder;
+
+public enum AnimalType
+{
+    Herbivore,
+    Carnivore
+}
+
+public class Animal
+{
+    public string Name { get; set; }
+    public AnimalType AnimalType { get; set; }
+}
+
+public abstract class FunctionalBuilder<TEntity, TSelf>
+    where TSelf: FunctionalBuilder<TEntity, TSelf>
+    where TEntity : new()
+{
+    private readonly List<Func<TEntity, TEntity>> actions = [];
+
+    private TSelf AddAction(Action<TEntity> action)
+    {
+        actions.Add(x => {  
+            action(x);
+            return x;
+        });
+
+        return (TSelf)this;
+    }
+
+    public TSelf Do(Action<TEntity> action) => AddAction(action);
+
+    public TEntity Build()
+    {
+        var entity = Activator.CreateInstance(typeof(TEntity));
+        actions.Aggregate(entity, static (p, f) => f((TEntity)p));
+
+        return (TEntity)entity;
+    }
+}
