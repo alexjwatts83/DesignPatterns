9854a404b2fb3c39e4d8dbe91ceb63a7afc6c44b
WIN-F7470BLT8P0\kamrui-pc <alexjwatts83@gmail.com>
2025-02-24 15:59:24 +1100

    Added FluentRecursiveBuilderTests

---
+++ src/DesignPatterns.Domain.Tests/Creational/AbstractFactoryPattern/FluentRecursiveBuilderTests.cs
@@ -0,0 +1,25 @@
+﻿using Xunit;
+using Shouldly;
+
+namespace DesignPatterns.Domain.Creational.AbstractFactoryPattern.Tests;
+
+public class FluentRecursiveBuilderTests
+{
+    [Fact()]
+    public void BuildTest()
+    {
+        // arrange
+        // act
+        var dob = new DateTime(1975, 7, 1);
+        var result = Person.New
+          .Called("Clark Kent")
+          .WorksAsA("Daily Planet")
+          .Born(dob)
+          .Build();
+
+        // assert
+        result.Name.ShouldBe("Clark Kent");
+        result.Position.ShouldBe("Daily Planet");
+        result.DateOfBirth.ShouldBe(dob);
+    }
+}
