a0915f6cbe1e852e0ca750e7b26626586cad5181
WIN-F7470BLT8P0\kamrui-pc <alexjwatts83@gmail.com>
2025-02-26 10:54:43 +1100

    Added abstract factory

---
+++ src/DesignPatterns.Domain.Tests/Creational/AbstractFactoryPattern/AbstractFactoryTests.cs
@@ -0,0 +1,29 @@
+﻿using DesignPatterns.Domain.Creational.AbstractFactoryPattern;
+
+namespace DesignPatterns.Domain.Tests.Creational.AbstractFactoryPattern;
+
+public class AbstractFactoryTests
+{
+    [Theory]
+    [InlineData("Coffee")]
+    [InlineData("Tea")]
+    public void MakeDrinkTest(string type)
+    {
+        // arrange
+        var machine = new HotDrinkMachine();
+
+        // act
+        var result = machine.MakeDrink(type, 100);
+
+        // assert
+        result.ShouldNotBeNull();
+        if (type == "Coffee")
+        {
+            result.ShouldBeOfType<Coffee>();
+        }
+        if (type == "Tea")
+        {
+            result.ShouldBeOfType<Tea>();
+        }
+    }
+}
---
+++ src/DesignPatterns.Domain/Creational/AbstractFactoryPattern/AbstractFactory.cs
@@ -0,0 +1,84 @@
+﻿namespace DesignPatterns.Domain.Creational.AbstractFactoryPattern;
+
+public interface IHotDrink
+{
+    string Name { get; }
+    void Consume();
+}
+
+internal class Tea : IHotDrink
+{
+    public string Name { get => nameof(Tea); }
+    public void Consume()
+    {
+        Console.WriteLine("This tea is nice but I'd prefer it with milk.");
+    }
+}
+
+internal class Coffee : IHotDrink
+{
+    public string Name { get => nameof(Coffee); }
+    public void Consume()
+    {
+        Console.WriteLine("This coffee is delicious!");
+    }
+}
+
+public interface IHotDrinkFactory
+{
+    IHotDrink Prepare(int amount);
+}
+
+internal class TeaFactory : IHotDrinkFactory
+{
+    public IHotDrink Prepare(int amount)
+    {
+        Console.WriteLine($"Put in tea bag, boil water, pour {amount} ml, add lemon, enjoy!");
+        return new Tea();
+    }
+}
+
+internal class CoffeeFactory : IHotDrinkFactory
+{
+    public IHotDrink Prepare(int amount)
+    {
+        Console.WriteLine($"Grind some beans, boil water, pour {amount} ml, add cream and sugar, enjoy!");
+        return new Coffee();
+    }
+}
+
+public class HotDrinkMachine
+{
+    private readonly Dictionary<string, IHotDrinkFactory> namedFactories = [];
+
+    public HotDrinkMachine()
+    {
+        //foreach (AvailableDrink drink in Enum.GetValues(typeof(AvailableDrink)))
+        //{
+        //  var factory = (IHotDrinkFactory) Activator.CreateInstance(
+        //    Type.GetType("DotNetDesignPatternDemos.Creational.AbstractFactory." + Enum.GetName(typeof(AvailableDrink), drink) + "Factory"));
+        //  factories.Add(drink, factory);
+        //}
+
+        foreach (var t in typeof(HotDrinkMachine).Assembly.GetTypes())
+        {
+            if (typeof(IHotDrinkFactory).IsAssignableFrom(t) && !t.IsInterface)
+            {
+                var name = t.Name.Replace("Factory", string.Empty);
+                var factory = Activator.CreateInstance(t) as IHotDrinkFactory;
+                namedFactories.Add(name, factory!);
+            }
+        }
+    }
+
+    public IHotDrink MakeDrink(string type, int amount)
+    {
+        if (!namedFactories.ContainsKey(type))
+            throw new InvalidOperationException($"Drink type of '{type}' is not a valid type");
+
+        if (amount < 0)
+            throw new InvalidOperationException($"Amount must be greater than 0 but was '{amount}'");
+
+        return namedFactories[type].Prepare(amount);
+    }
+}
