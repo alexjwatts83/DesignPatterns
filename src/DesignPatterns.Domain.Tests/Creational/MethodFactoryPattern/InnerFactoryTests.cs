23623fc0bc1166372396f511371093cdfdcc8796
WIN-F7470BLT8P0\kamrui-pc <alexjwatts83@gmail.com>
2025-02-25 16:12:59 +1100

    Added internal factory and tests

---
+++ src/DesignPatterns.Domain.Tests/Creational/MethodFactoryPattern/InnerFactoryTests.cs
@@ -0,0 +1,30 @@
+﻿using DesignPatterns.Domain.Creational.MethodFactoryPattern;
+
+namespace DesignPatterns.Domain.Tests.Creational.MethodFactoryPattern;
+
+public class InnerFactoryTests
+{
+    [Fact()]
+    public void NewCartesianPointTest()
+    {
+        // arrange
+        // act
+        var point = Point3.Factory.NewCartesianPoint(100, 200);
+
+        // assert
+        point.X.ShouldBe(100);
+        point.Y.ShouldBe(200);
+    }
+
+    [Fact()]
+    public void NewPolarPointTest()
+    {
+        // arrange
+        // act
+        var point = Point3.Factory.NewPolarPoint(100, 200);
+
+        // assert
+        point.X.ShouldBe(100 * Math.Cos(200));
+        point.Y.ShouldBe(100 * Math.Sin(200));
+    }
+}
---
+++ src/DesignPatterns.Domain/Creational/MethodFactoryPattern/InternalFactory.cs
@@ -0,0 +1,23 @@
+﻿namespace DesignPatterns.Domain.Creational.MethodFactoryPattern;
+
+public class Point3
+{
+    public double X { get; set; }
+    public double Y { get; set; }
+
+    private Point3(double x, double y)
+    {
+        X = x;
+        Y = y;
+    }
+
+    public static class Factory
+    {
+        public static Point3 NewCartesianPoint(double x, double y) => new(x, y);
+
+        public static Point3 NewPolarPoint(double rho, double theta)
+        {
+            return new Point3(rho * Math.Cos(theta), rho * Math.Sin(theta));
+        }
+    }
+}
