c9e666ffad3f69ecd2aa5ce9f8cd3bdfb52a653b
WIN-F7470BLT8P0\kamrui-pc <alexjwatts83@gmail.com>
2025-02-25 11:39:05 +1100

    Added FacetedBuilder and FacetedBuilderTests

---
+++ src/DesignPatterns.Domain.Tests/Creational/Builder/FacetedBuilderTests.cs
@@ -0,0 +1,36 @@
+﻿using DesignPatterns.Domain.Creational.Builder;
+
+namespace DesignPatterns.Domain.Tests.Creational.Builder;
+
+public class FacetedBuilderTests
+{
+    [Fact()]
+    public void EarningTest()
+    {
+        // arrange
+        // act
+        var builder = new CitizenBuilder();
+        Citizen result = builder
+            .Named("King Charles")
+            .Lives
+                .At("123 London Road")
+                .In("London")
+                .WithPostcode("SW12BC")
+            .Works
+                .At("Fabrikam")
+                .AsA("Engineer")
+                .Earning(123000);
+
+        // assert
+        result.ShouldNotBeNull();
+        result.Name.ShouldBe("King Charles");
+        // address
+        result.StreetAddress.ShouldBe("123 London Road");
+        result.Postcode.ShouldBe("SW12BC");
+        result.City.ShouldBe("London");
+        // employment
+        result.CompanyName.ShouldBe("Fabrikam");
+        result.Position.ShouldBe("Engineer");
+        result.AnnualIncome.ShouldBe(123000);
+    }
+}
---
+++ src/DesignPatterns.Domain/Creational/Builder/FacetedBuilder.cs
@@ -0,0 +1,95 @@
+﻿namespace DesignPatterns.Domain.Creational.Builder;
+
+public class Citizen
+{
+    public string Name { get; set; } = string.Empty;
+
+    // address
+    public string StreetAddress { get; set; } = string.Empty;
+    public string Postcode { get; set; } = string.Empty;
+    public string City { get; set; } = string.Empty;
+
+    // employment
+    public string CompanyName { get; set; } = string.Empty;
+    public string Position { get; set; } = string.Empty;
+
+    public int AnnualIncome { get; set; }
+
+    public override string ToString()
+    {
+        return $"{nameof(Name)}: {Name}, {nameof(StreetAddress)}: {StreetAddress}, {nameof(Postcode)}: {Postcode}, {nameof(City)}: {City}, {nameof(CompanyName)}: {CompanyName}, {nameof(Position)}: {Position}, {nameof(AnnualIncome)}: {AnnualIncome}";
+    }
+}
+
+public class CitizenBuilder // facade 
+{
+    // the object we're going to build
+    protected Citizen citizen = new(); // this is a reference!
+
+    public CitizenBuilder Named(string name)
+    {
+        citizen.Name = name;
+        return this;
+    }
+
+    public CitizenAddressBuilder Lives => new(citizen);
+    public CitizenJobBuilder Works => new(citizen);
+
+    public static implicit operator Citizen(CitizenBuilder cb)
+    {
+        return cb.citizen;
+    }
+}
+
+public class CitizenJobBuilder : CitizenBuilder
+{
+    public CitizenJobBuilder(Citizen citizen)
+    {
+        this.citizen = citizen;
+    }
+
+    public CitizenJobBuilder At(string companyName)
+    {
+        citizen.CompanyName = companyName;
+        return this;
+    }
+
+    public CitizenJobBuilder AsA(string position)
+    {
+        citizen.Position = position;
+        return this;
+    }
+
+    public CitizenJobBuilder Earning(int annualIncome)
+    {
+        citizen.AnnualIncome = annualIncome;
+        return this;
+    }
+}
+
+public class CitizenAddressBuilder : CitizenBuilder
+{
+    // might not work with a value type!
+    public CitizenAddressBuilder(Citizen Citizen)
+    {
+        this.citizen = Citizen;
+    }
+
+    public CitizenAddressBuilder At(string streetAddress)
+    {
+        citizen.StreetAddress = streetAddress;
+        return this;
+    }
+
+    public CitizenAddressBuilder WithPostcode(string postcode)
+    {
+        citizen.Postcode = postcode;
+        return this;
+    }
+
+    public CitizenAddressBuilder In(string city)
+    {
+        citizen.City = city;
+        return this;
+    }
+}
