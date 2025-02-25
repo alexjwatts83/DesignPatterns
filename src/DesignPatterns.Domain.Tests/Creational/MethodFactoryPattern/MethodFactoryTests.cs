f4a7f632426f132112fb4158e1ecea98b9f316db
WIN-F7470BLT8P0\kamrui-pc <alexjwatts83@gmail.com>
2025-02-25 15:43:19 +1100

    wip checkin

---
+++ src/DesignPatterns.Domain.Tests/Creational/MethodFactoryPattern/MethodFactoryTests.cs
@@ -0,0 +1,28 @@
+﻿namespace DesignPatterns.Domain.Creational.MethodFactoryPattern.Tests;
+
+public class MethodFactoryTests
+{
+    [Fact()]
+    public void NewCartesianPointTest()
+    {
+        // arrange
+        // act
+        var cartesianPoint = Point.NewCartesianPoint(100, 200);
+
+        // assert
+        cartesianPoint.X.ShouldBe(100);
+        cartesianPoint.Y.ShouldBe(200);
+    }
+
+    [Fact()]
+    public void NewPolarPointTest()
+    {
+        // arrange
+        // act
+        var cartesianPoint = Point.NewPolarPoint(100, 200);
+
+        // assert
+        cartesianPoint.X.ShouldBe(100 * Math.Cos(200));
+        cartesianPoint.Y.ShouldBe(100 * Math.Sin(200));
+    }
+}
---
+++ src/DesignPatterns.Domain/Creational/MethodFactoryPattern/MethodFactory.cs
@@ -0,0 +1,49 @@
+﻿namespace DesignPatterns.Domain.Creational.MethodFactoryPattern;
+
+public enum CoordinateSystem
+{
+    Cartesian,
+    Polar
+}
+
+public class Point
+{
+    public double X { get; set; }
+    public double Y { get; set; }
+
+    protected Point(double x, double y)
+    {
+        X = x;
+        Y = y;
+    }
+
+    //public Point(double a, double b, // names do not communicate intent
+    //    CoordinateSystem cs = CoordinateSystem.Cartesian)
+    //{
+    //    switch (cs)
+    //    {
+    //        case CoordinateSystem.Polar:
+    //            X = a * Math.Cos(b);
+    //            Y = a * Math.Sin(b);
+    //            break;
+    //        default:
+    //            X = a;
+    //            Y = b;
+    //            break;
+    //    }
+
+    //    // steps to add a new system
+    //    // 1. augment CoordinateSystem
+    //    // 2. change ctor
+    //}
+
+    public static Point NewCartesianPoint(double x, double y)
+    {
+        return new Point(x, y);
+    }
+
+    public static Point NewPolarPoint(double rho, double theta)
+    {
+        return new Point(rho * Math.Cos(theta), rho * Math.Sin(theta));
+    }
+}
