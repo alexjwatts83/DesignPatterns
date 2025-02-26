edcb94e4e7cf2c3b991942693d9167ab7432cc63
WIN-F7470BLT8P0\kamrui-pc <alexjwatts83@gmail.com>
2025-02-26 15:55:14 +1100

    Added PrototypeInheritance and PrototypeInheritanceTests

---
+++ src/DesignPatterns.Domain.Tests/Creational/PrototypePattern/PrototypeInheritanceTests.cs
@@ -0,0 +1,35 @@
+﻿using DesignPatterns.Domain.Creational.PrototypePattern;
+
+namespace DesignPatterns.Domain.Tests.Creational.PrototypePattern;
+
+public class PrototypeInheritanceTests
+{
+    [Fact()]
+    public void ToStringTest()
+    {
+        // arrange
+        var john = new Employee2
+        {
+            Names = ["John", "Doe"],
+            Address = new Address2 { HouseNumber = 123, StreetName = "London Road" },
+            Salary = 321000
+        };
+        var copy = john.DeepCopy();
+
+        // act
+        copy.Names[1] = "Smith";
+        copy.Address.HouseNumber = 456;
+        copy.Salary = 123000;
+
+        // assert
+        john.Name.ShouldBe("John Doe");
+        john.Address.HouseNumber.ShouldBe(123);
+        john.Address.StreetName.ShouldBe("London Road");
+        john.Salary.ShouldBe(321000);
+
+        copy.Name.ShouldBe("John Smith");
+        copy.Address.HouseNumber.ShouldBe(456);
+        copy.Address.StreetName.ShouldBe("London Road");
+        copy.Salary.ShouldBe(123000);
+    }
+}
---
+++ src/DesignPatterns.Domain/Creational/PrototypePattern/PrototypeInheritance.cs
@@ -0,0 +1,103 @@
+﻿namespace DesignPatterns.Domain.Creational.PrototypePattern;
+
+public interface IDeepCopyable<T> where T : new()
+{
+    void CopyTo(T target);
+
+    // new to C#
+    public T DeepCopy()
+    {
+        T t = new T();
+        CopyTo(t);
+        return t;
+    }
+}
+
+public static class DeepCopyExtensions
+{
+    public static T DeepCopy<T>(this IDeepCopyable<T> item)
+      where T : new()
+    {
+        return item.DeepCopy();
+    }
+
+    public static T DeepCopy<T>(this T person)
+      where T : Person, new()
+    {
+        return ((IDeepCopyable<T>)person).DeepCopy();
+    }
+}
+
+public class Address2 : IDeepCopyable<Address2>
+{
+    public string StreetName;
+    public int HouseNumber;
+
+    //public Address2(string streetName, int houseNumber)
+    //{
+    //    StreetName = streetName;
+    //    HouseNumber = houseNumber;
+    //}
+
+    //public Address2()
+    //{
+
+    //}
+
+    public override string ToString()
+    {
+        return $"{nameof(StreetName)}: {StreetName}, {nameof(HouseNumber)}: {HouseNumber}";
+    }
+
+    public void CopyTo(Address2 target)
+    {
+        target.StreetName = StreetName;
+        target.HouseNumber = HouseNumber;
+    }
+}
+
+public class Person : IDeepCopyable<Person>
+{
+    public string[] Names;
+    public Address2 Address;
+
+    public string Name { get { return string.Join(" ", Names); } }
+
+    //public Person()
+    //{
+
+    //}
+
+    //public Person(string[] names, Address2 address)
+    //{
+    //    Names = names;
+    //    Address = address;
+    //}
+
+    public override string ToString()
+    {
+        return $"{nameof(Names)}: {string.Join(",", Names)}, {nameof(Address)}: {Address}";
+    }
+
+    public virtual void CopyTo(Person target)
+    {
+        target.Names = (string[])Names.Clone();
+        target.Address = Address.DeepCopy();
+    }
+}
+
+public class Employee2 : Person, IDeepCopyable<Employee2>
+{
+    public int Salary;
+
+    public void CopyTo(Employee2 target)
+    {
+        base.CopyTo(target);
+        target.Salary = Salary;
+    }
+
+    public override string ToString()
+    {
+        return $"{base.ToString()}, {nameof(Salary)}: {Salary}";
+    }
+}
